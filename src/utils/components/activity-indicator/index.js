7249476b5e085074d3884ff41916e84b3652a8a1
Henrique Ladeira <henrique.c.ladeira@gmail.com>
2021-06-05 13:37:25 -0300

    fix: layout lottie animation

--- src/utils/components/activity-indicator/index.js
+++ src/utils/components/activity-indicator/index.js
@@ -4,5 +4,5 @@ import Lottie from 'react-lottie-player';
 import lottieJson from './lottie-source.json';
 
 export const ActivityIndicator = () => (
-  <Lottie loop animationData={lottieJson} play style={{ width: 300, height: 300 }} />
+  <Lottie loop animationData={lottieJson} play style={{ width: 300, height: 300, margin: 'auto' }} />
 );
