e864998daea52e4bff4a292a7568316457c96481
Henrique Ladeira <henrique.c.ladeira@gmail.com>
2021-06-05 13:43:56 -0300

    chore: config redux dev tools for browser ext

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,9 @@
-import { applyMiddleware, createStore } from 'redux';
+/* eslint-disable no-underscore-dangle */
+import { applyMiddleware, compose, createStore } from 'redux';
 import thunk from 'redux-thunk';
 import { rootReducer } from './ducks';
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
 
 export default store;
