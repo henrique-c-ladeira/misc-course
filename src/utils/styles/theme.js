43e2ceb0c15a060c422eed8a16f128e0c60ca08c
Henrique Ladeira <henrique.c.ladeira@gmail.com>
2021-06-05 14:25:16 -0300

    feat: add appTheme colors

---
+++ src/utils/styles/theme.js
@@ -0,0 +1,13 @@
+const appTheme = {
+  colors: {
+    black: '#010a00',
+    white: '#ffffff',
+    lightBlue: '#B8D8EE',
+    darkBlue: '#6E54D6',
+    lightGray: '#C4C4C4',
+    darkGrey: '#737373',
+    red: '#D64C46',
+  },
+};
+
+export default appTheme;
