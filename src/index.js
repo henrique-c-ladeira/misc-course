b37355d28cc99a9c4ef4050bd816115ada42a954
Henrique Ladeira <henrique.c.ladeira@gmail.com>
2021-06-05 11:55:35 -0300

    feat: add store and provider

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import './index.css';
 import Routes from './routes';
+import store from './store';
 
-ReactDOM.render(<Routes />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <Routes />
+  </Provider>,
+  document.getElementById('root'),
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
